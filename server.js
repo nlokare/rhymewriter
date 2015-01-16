2674bb61ac4ee4c183fbee73cf10c67ac4d8f9d2
Neil Lokare <neil.lokare@gmail.com>
2015-01-15 19:07:10 -0800

    Successfully sends rhyming words to the client for a word passed as a URL path

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 var express = require('express');
 var Generator = require('./rhymeGenerator/rhymeGenerator.js');
+var _ = require('underscore');
 
 var app = express();
 
-app.get('/rhyme', function (req, res) {
-  Generator.getRhymes(req.body)
+app.listen(4568);
+
+app.get('/:rhyme', function (req, res) {
+  Generator.getRhymes(req.params.rhyme)
     .then(Generator.objectifyRhymes)
     .then(function (data) {
-      res.send(Object.keys(data));
+      var rhymes = Object.keys(data);
+      var words = [];
+      _.each(rhymes, function (word) {
+        word = '<p>' + word + '</p>';
+        words.push(word);
+      });
+      res.send(words.join(''));
     })
     .catch(function (err) {
       console.log(err);
     });
 });
-
-Generator.getRhymes("focus")
-    .then(Generator.objectifyRhymes)
-    .then(function (data) {
-      console.log(Object.keys(data));
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
